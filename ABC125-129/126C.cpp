d3f49ca6a34305f1d388fb8c8edca8d346b17875
Ikumi Ito <ikumi.ito.p8@dc.tohoku.ac.jp>
2020-12-28 03:25:10 +0900

    Add files via upload

---
+++ ABC125-129/126C.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long double ll;
+typedef unsigned long long ull;
+const long long INF = 1LL << 60;
+const int MOD = 1000000007;
+
+int main() {
+  ll N, K;
+  cin >> N >> K;
+  ll res = 0;
+  for (int i = 1; i <= N; i++) {
+    ll out = 1;
+    ll tmp = i;
+    while (tmp < K) {
+      tmp *= 2;
+      out *= 0.5;
+    }
+    res += out/N;
+  }
+  cout << fixed << setprecision(11) << res << endl;
+  return 0;
+}
