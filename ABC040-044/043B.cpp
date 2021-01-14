0e088a5587dcd201b45b087019b689ae2f39d372
Ikumi Ito <ikumi.ito.p8@dc.tohoku.ac.jp>
2021-01-15 00:21:00 +0900

    Add files via upload

---
+++ ABC040-044/043B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define rep(i, n) for (ll i = 0; i < n; ++i)
+#define rep_up(i, a, b) for (ll i = a; i < b; ++i)
+#define rep_down(i, a, b) for (ll i = a; i > b; --i)
+
+#define fi first
+#define se second
+
+#define vvvvll vector<vector<vector<vector<ll>>>>
+#define vvvll vector<vector<vector<ll>>>
+#define vvll vector<vector<ll>>
+#define vll vector<ll>
+
+#define pqmax priority_queue<ll>
+#define pqmin priority_queue<ll, vector<ll>, greater<ll>>
+
+constexpr ll INF = (1ll << 60);
+constexpr ll mod = 1000000007;
+constexpr double pi = 3.14159265358979323846;
+
+using ping = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+
+const ll dx[4] = {1, 0, -1, 0};
+const ll dy[4] = {0, 1, 0, -1};
+
+template <typename T>
+bool chmax(T& a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+template <typename T>
+bool chmin(T& a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  ll N = s.size();
+  vector<char> out;
+  for(ll i = 0; i < N; i++){
+      if(s[i] == '0'){
+        out.push_back(s[i]);
+      }
+      else if(s[i] == '1'){
+        out.push_back(s[i]);
+      }
+      else{
+        if (out.size() == 0) continue;
+        out.pop_back();
+      }
+  }
+  for(ll i = 0; i < out.size(); i++){
+    cout << out[i];
+  }
+  cout << endl;
+  return 0;
+}
